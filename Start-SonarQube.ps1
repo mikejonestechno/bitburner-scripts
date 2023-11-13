1c60750ae1e39d2104a2533adcc1d2a5f8aabefe
Mike Jones <mikejonestechno@users.noreply.github.com>
2023-11-13 18:21:20 +1100

    feat: Add  popup reminder to disable VPN during SonarQube container scan

--- Start-SonarQube.ps1
+++ Start-SonarQube.ps1
@@ -13,9 +13,7 @@ if ($null -eq (Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue)
 # Set the SONAR_LOGIN environment variable to the SonarQube user token
 #-e SONAR_LOGIN="sqp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
 
-#
-# ?? do i need to disable surfshark and then retry staring the container so it can get its connection to ds920?
-#
+
 
 $containerName = "sonar-scanner"
 Write-Output "Starting $containerName container..."
@@ -25,3 +23,6 @@ if ($containers | Select-String -Pattern "^$containerName$" -Quiet) {
 } else {
   docker run -e SONAR_HOST_URL="http://ds920.local.disrupting.technology:9000" -e SONAR_LOGIN="${ENV:SONAR_LOGIN}" -e SONAR_VERBOSE=true -v "${PWD}:/usr/src" -v "sonar-cache:/opt/sonar-scanner/.sonar/cache" --name $containerName sonarsource/sonar-scanner-cli
 }
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show('Remember to PAUSE the VPN to enable container API access to the NAS.',"VPN Connection",[System.Windows.Forms.MessageBoxButtons]::OK)
