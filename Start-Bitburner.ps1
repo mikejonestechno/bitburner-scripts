d21088e2a6b6533bc9cc8ed0f7d595ffe1880a9d
Mike Jones <mikejonestechno@users.noreply.github.com>
2023-12-02 02:12:45 +0000

    Open vscode in WSL

--- Start-Bitburner.ps1
+++ Start-Bitburner.ps1
@@ -11,22 +11,29 @@ if ($null -eq (Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue)
 
 # If VS Code not running start VS Code
 # IF VS Code devcontainer extension is installed then start VS Code with devcontainer.json
+# Clone bitburner-scripts repo into VS Code devcontainer...
+# https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/mikejonestechno/bitburner-scripts
+
 if ($null -eq (Get-Process -Name "Visual Studio Code" -ErrorAction SilentlyContinue)) {
-    Write-Output "Starting VSCode..."
-    #Start-Process -FilePath "C:\Program Files\Microsoft VS Code\code.exe" -ArgumentList "./devcontainer/devcontainer.json"
-    Start-Process -FilePath "C:\Program Files\Microsoft VS Code\code.exe" -ArgumentList "./.vscode/bitburner-scripts.code-workspace"
+    Write-Output "Starting VSCode WSL..."
+
+    # https://code.visualstudio.com/docs/remote/wsl
+    $arglist = "--remote wsl+Ubuntu /home/wsluser/github/bitburner-scripts";
+    # or $arglist = "--folder-uri vscode-remote://wsl+Ubuntu/home/wsluser/github/bitburner-scripts"
+    Start-Process -FilePath "C:\Program Files\Microsoft VS Code\code.exe" -ArgumentList $arglist
+
+    # if devcontainer CLI extension is installed via VSCode UI (f1)
+    # Start-Process -FilePath "devcontainer" -ArgumentList "open \\wsl.localhost\Ubuntu\home\wsluser\github\bitburner-scripts"
+
 } else {
     Write-Output "VS Code process already running."
 }
-
-# Clone bitburner-scripts repo into VS Code devcontainer...
-# https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/mikejonestechno/bitburner-scripts
 
 
 # If bitburner not running start bitburner
 if ($null -eq (Get-Process -Name "bitburner" -ErrorAction SilentlyContinue)) {
     Write-Output "Starting bitburner process..."
-    start-sleep -Seconds 3 # allow time for for bitburner-filesync to be in healthy state
+    start-sleep -Seconds 5 # allow time for for bitburner-filesync to be in healthy state
     Start-Process -Wait -FilePath "C:\Program Files (x86)\Steam\steamapps\common\Bitburner\bitburner.exe"
 } else {
     Write-Output "Bitburner process already running. Sleeping till bitburner terminates..."
